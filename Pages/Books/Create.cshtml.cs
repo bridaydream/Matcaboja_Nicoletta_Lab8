a493cb7107efa456ff0e0093925c8b3306e75d42
Nicoletta <nikodani.bb@gmail.com>
2022-01-19 17:13:16 +0200

    .

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Matcaboja_Nicoletta_Lab8.Models;
 
 namespace Matcaboja_Nicoletta_Lab8.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel :  : BookCategoriesPageModel
     {
         private readonly Matcaboja_Nicoletta_Lab8.Data.Matcaboja_Nicoletta_Lab8Context _context;
 
@@ -22,14 +22,48 @@ namespace Matcaboja_Nicoletta_Lab8.Pages.Books
         public IActionResult OnGet()
         {
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
-            return Page();
+
+            var book = new Book();
+             book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
+
+        return Page();
         }
 
         [BindProperty]
         public Book Book { get; set; }
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+    public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
+    {
+        var newBook = new Book();
+        if (selectedCategories != null)
+        {
+            newBook.BookCategories = new List<BookCategory>();
+            foreach (var cat in selectedCategories)
+            {
+                var catToAdd = new BookCategory
+                {
+                    CategoryID = int.Parse(cat)
+                };
+                newBook.BookCategories.Add(catToAdd);
+            }
+        }
+        if (await TryUpdateModelAsync<Book>(
+        newBook,
+        "Book",
+        i => i.Title, i => i.Author,
+        i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+        {
+            _context.Book.Add(newBook);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+        PopulateAssignedCategoryData(_context, newBook);
+        return Page();
+    }
+
+    // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+    public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
